77af24a30755a759c304592b8d64cec3d7704d2f
Tuviccfp <victorcordeirofp@gmail.com>
2025-07-28 03:39:57 -0300

    init product controller & model

---
+++ src/controller/product/index.ts
@@ -0,0 +1,40 @@
+import { Router, Request, Response } from 'express';
+import {authenticated, validRole} from "../../middleware";
+import {Types} from "mongoose";
+import log from "../../logger";
+import {Product} from "../../models/product";
+
+const router = Router();
+
+router.post("/register", [authenticated, validRole], async (req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined> => {
+    const user: {_id: string | Types.ObjectId, role: string} | undefined = req.user;
+    if(!user?._id && !user?.role) {
+        log.warn({_id: user?._id}, "Credênciais inválidas, permissão negada!");
+        return res.status(401).json({
+            message: "Credênciais inválidas, permissão negada!"
+        })
+    }
+    try {
+        const {name, price, description, image} = req.body;
+        if(!name || !price || !description || !image) {
+            return res.status(400).json({
+                message: "Não é permitido campos vazios"
+            })
+        }
+        await Product.create({
+            name,
+            price,
+            description,
+            image,
+            userCreate: user._id,
+        })
+        log.info({name: name, userCreate: user._id}, "Produto cadastrado com sucesso!");
+        return res.status(200).json({
+            message: "Produto cadastrado com sucesso!"
+        });
+    } catch (e) {
+        log.info({e: e}, "Erro ao cadastrar produto");
+    }
+});
+
+export default router;
---
+++ src/models/product/index.ts
@@ -0,0 +1,27 @@
+import mongoose, {Schema, Document, Types} from "mongoose";
+
+interface Product extends Document {
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+    userCreate?: Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const productSchema = new Schema<Product>({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    userCreate: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+export const Product =  mongoose.model<Product>("Product", productSchema);
