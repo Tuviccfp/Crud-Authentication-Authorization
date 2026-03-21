c3ab55cc36f3910fb4a1ee5d25a3f9e3901d480a
victo <victorcordeirofp@gmail.com>
2026-03-21 14:33:56 -0300

    refactor: retry exponencial p/ conexao mongo

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
-export function connectDatabase(): void {
-    if (process.env.MONGODB_URL === undefined) throw new Error(
+import {NextFunction} from "express";
+import log from "../logger";
+export async function connectDatabase(retries: 5, delay: 1000): Promise<void> {
+    if (!process.env.MONGODB_URL) throw new Error(
         "Please, set the MONGODB_URL environment variable"
     );
-    mongoose.connect(process.env.MONGODB_URL)
-        .then(() => console.log("Database connected"))
-        .catch((e) => {
-            console.log(e)
-        });
+    for(let attempt = 0; attempt <= retries; attempt++) {
+        try {
+            await mongoose.connect(process.env.MONGODB_URL as string);
+            log.info("Conectado ao banco de dados");
+            return;
+        }
+        catch (e) {
+            log.error(`Erro ao se conectar com o mongoDB, ${e}, tentativa atual: ${attempt} de ${retries}`);
+            if(attempt === retries) throw new Error("Erro ao se conectar com o mongoDB");
+            const waitTime = delay * 2 ** (attempt - 1);
+            await new Promise(resolve => setTimeout(resolve, waitTime));
+        }
+    }
 }
