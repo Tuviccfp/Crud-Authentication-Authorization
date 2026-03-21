d24889574f92fa652c1a2170ef9d943cb647876b
victo <victorcordeirofp@gmail.com>
2026-03-21 17:24:29 -0300

    refactor: timestamp do mongo, mensagens de erro pros casos do zod

--- src/models/product/index.ts
+++ src/models/product/index.ts
@@ -1,26 +1,26 @@
-import mongoose, {Schema, Document, Types} from "mongoose";
+import mongoose, {Schema, Document, Types, ObjectId} from "mongoose";
+import {z, ZodType} from "zod";
 
-export interface ProductTypes {
+const productSchemaValidation = z.object({
+    name: z.string({
+        message: "O nome é obrigatório"
+    }).min(5, "É necessário ter no mínimo 5 caracteres").max(50, "Não pode ultrapassar 50 caracteres"),
+    price: z.number({
+        message: "Preço não pode ser vazio nem negativo"
+    }).min(0, "O preço não pode ser negativo"),
+    description: z.string().min(5, "É necessário ter no mínimo 5 caracteres").max(255, "Não pode ultrapassar 255 caracteres"),
+    image: z.string().min(5, "").max(255, ""),
+});
+
+export interface ProductDocument extends Document {
     name: string;
     price: number;
     description: string;
     image: string;
-    updatedAt?: Date;
-    createdAt?: Date;
-    userCreate?: Types.ObjectId;
+    userCreate: Types.ObjectId;
 }
 
-export interface Product extends Document {
-    name: string;
-    price: number;
-    description: string;
-    image: string;
-    userCreate?: Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
-}
-
-const productSchema = new Schema<Product>({
+const productSchema = new Schema<ProductDocument>({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
@@ -29,9 +29,8 @@ const productSchema = new Schema<Product>({
         type: Schema.Types.ObjectId,
         ref: "User",
         required: true
-    },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-});
+    }
+}, { timestamps: true });
 
-export const Product =  mongoose.model<Product>("Product", productSchema);
+export const Product =  mongoose.model<ProductDocument>("Product", productSchema);
+export { productSchemaValidation }
--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import { NextFunction } from "express";
+import {z} from "zod";
 
-export interface UserTypes {
-    name: string;
-    email: string;
-    password: string;
-    role?: string;
-}
+export const userSchemaValidation = z.object({
+    name: z.string().min(5, "O nome deve conter ao menos 5 caracteres").max(30, "O nome deve conter até 30 caracteres"),
+    email: z.email("Email inválido"),
+    password: z.string().min(8, "A senha deve conter ao menos 8 caracteres"),
+    role: z.enum(["basic", "employeer", "admin"]).default("basic").optional(),
+})
+
+export type UserTypes = z.infer<typeof userSchemaValidation>;
 
 export interface UserDocument extends UserTypes, Document {
     checkPassword: (password: string) => Promise<boolean>;
@@ -18,8 +21,8 @@ const userSchema = new Schema<UserDocument>({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true, default: "basic" },
-});
+    role: { type: String, required: true, enum: ["basic", "employeer", "admin"],default: "basic" }
+}, { timestamps: true });
 
 userSchema.pre("save", async function (next): Promise<void> {
     if(!this.isModified("password")) return next();
