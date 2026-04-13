e2de9486b3f8fed34f94b267e3da3e7938e4aaa6
victo <victorcordeirofp@gmail.com>
2026-04-12 22:20:44 -0300

    refactor: separando product para monorepo

--- src/models/product/index.ts
+++ src/models/product/index.ts
@@ -1,26 +1,5 @@
-import mongoose, {Schema, Document, Types, ObjectId} from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import { ProductDocument } from "@projeto/shared/interfaces/product"
-import { z } from "zod";
-// const productSchemaValidation = z.object({
-//     name: z.string({
-//         message: "O nome é obrigatório"
-//     }).min(5, "É necessário ter no mínimo 5 caracteres").max(50, "Não pode ultrapassar 50 caracteres"),
-//     price: z.number({
-//         message: "Preço não pode ser vazio nem negativo"
-//     }).min(0, "O preço não pode ser negativo"),
-//     description: z.string().min(5, "É necessário ter no mínimo 5 caracteres").max(255, "Não pode ultrapassar 255 caracteres"),
-//     image: z.string().min(5, "").max(255, ""),
-//     status: z.enum(["ativo", "inativo"]).default("ativo"),
-// });
-
-// export interface ProductDocument extends Document {
-//     name: string;
-//     price: number;
-//     description: string;
-//     image: string;
-//     status: string;
-//     userCreate: Types.ObjectId;
-// }
 
 const productSchema = new Schema<ProductDocument>({
     name: { type: String, required: true },
@@ -36,4 +15,3 @@ const productSchema = new Schema<ProductDocument>({
 }, { timestamps: true });
 
 export const Product =  mongoose.model<ProductDocument>("Product", productSchema);
-// export { productSchemaValidation }
