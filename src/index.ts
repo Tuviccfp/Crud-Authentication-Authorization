c751e6b0a0effe48b5b867eec91b43cb7a5fde50
victo <victorcordeirofp@gmail.com>
2026-04-12 22:21:09 -0300

    refactor: colocando log no start do servidor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express, { Express } from "express";
+import express, {Express, Request, Response, NextFunction} from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieparser from "cookie-parser"
-
 import {connectDatabase} from "./config/database";
 import authRouter from "./router/auth.router";
 import productRouter from "./router/product.router";
 import userRouter from "./router/user.router";
+import log from "./logger";
+import {middlewareError} from "./middleware";
 
 const app: Express = express();
 
@@ -17,17 +18,24 @@ app.use(cors({
     origin: "http://localhost:3000",
     credentials: true,
 }));
-app.use(cookieparser());
-connectDatabase();
 
-app.use("/api/login", authRouter);
-app.use("/api/user", userRouter);
-app.use("/api/product", productRouter);
+app.use(cookieparser());
 
 app.get("/", (req, res) => {
     res.status(201).send("Hello World");
 });
 
+app.use("/api/login", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+
+app.use(middlewareError);
+
 app.listen(8080, (): void => {
-    console.log("App connected in port 3000");
+    try {
+        log.info("Servidor rodando na porta 8080");
+        connectDatabase(5, 1000);
+    } catch (e) {
+        log.error("Erro ao subir aplicação");
+    }
 });
